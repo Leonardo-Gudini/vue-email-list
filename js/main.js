e5de2d89f6ec66219790bbca7d14ecfd6ec58185
Leonardo Gudini <leonardo.gudini98@gmail.com>
2022-08-22 17:56:49 +0200

    fixed stampa mail

--- js/main.js
+++ js/main.js
@@ -5,15 +5,34 @@ var app = new Vue({
         emailCasuale: [],
     },
 
-    methods: {
+    // methods: {
 
-            generateMail() {
-                axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-                .then( mail => {
-                    this.emailCasuale.push(mail.data.response);
-                    // console.log(mail.data.response);
-                    // console.log(this.emailCasuale)
-                })
-            },
-    },
+    //         // generateMail() {
+                
+    //         // },
+    // },
+    // mounted(){
+    // let that = this;
+
+    
+    //     // axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+    //     //         .then( mail => {
+    //     //             that.emailCasuale.push(mail.data.response);
+    //     //             // console.log(mail.data.response);
+    //     //             // console.log(this.emailCasuale)
+    //     //         })
+    // }
+
+
+    mounted(){
+        for (let i=0; i<=9; i++){
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                        .then( mail => {
+                            this.emailCasuale.push(mail.data.response);
+                            // console.log(mail.data.response);
+                            // console.log(this.emailCasuale)
+                        })
+        }
+    }
 })
+
