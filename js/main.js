792014d02d51ad6a47f1d75d430660f1dacf5199
Leonardo Gudini <leonardo.gudini98@gmail.com>
2022-08-22 18:02:05 +0200

    eliminato codice commentato

--- js/main.js
+++ js/main.js
@@ -4,33 +4,11 @@ var app = new Vue({
     data:{
         emailCasuale: [],
     },
-
-    // methods: {
-
-    //         // generateMail() {
-                
-    //         // },
-    // },
-    // mounted(){
-    // let that = this;
-
-    
-    //     // axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-    //     //         .then( mail => {
-    //     //             that.emailCasuale.push(mail.data.response);
-    //     //             // console.log(mail.data.response);
-    //     //             // console.log(this.emailCasuale)
-    //     //         })
-    // }
-
-
     mounted(){
         for (let i=0; i<=9; i++){
             axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
                         .then( mail => {
                             this.emailCasuale.push(mail.data.response);
-                            // console.log(mail.data.response);
-                            // console.log(this.emailCasuale)
                         })
         }
     }
